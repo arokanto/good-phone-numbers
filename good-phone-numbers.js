5de0a42e3f47269c41b664a8731e76d73bde3d34
Tuomas Arokanto <tuomas.arokanto@gmail.com>
2021-10-22 12:46:36 +0300

    Clean up and modernize code

--- good-phone-numbers.js
+++ good-phone-numbers.js
@@ -1,56 +1,51 @@
 function generatePhoneNumbers(amount, start, end) {
-	var numbers = [], i;
+	let numbers = [], i;
 	for (i = 0; i < amount; i++) {
 		numbers[i] = String(Math.floor(Math.random() * (end - start)) + start + 1);
 	}
 	return numbers;
 }
 
-function calculateScore(numbers) {
+function calculateScores(numbers) {
+	const scored = numbers.map(number => {
+		return {
+			score: calculateScore(number),
+			num: number
+		}
+	})
 
-	var l = numbers.length;
-	var array = [];
-	var i, j, k, thisNumber, len, tempAr;
-	var sortedNumbers;
+	const sorted = scored.sort(compare)
 
-	// Store the numbers to an array of objects
-	for (i = 0; i < l; i++) {
-		array[i] = {}
-		array[i].num = numbers[i];
-		array[i].score = 0;
-	}
+	return sorted
+}
 
-	// Start counting
-	for (i = 0; i < l; i++) {
-		thisNumber = array[i].num;
-		len = thisNumber.length;
-		tempAr = [];
-		for (j = 0; j < len; j++) {
-			thisChar = thisNumber.charAt(j);
+function calculateScore(number) {
+	const len = number.length
+	let score = 0
+	tempAr = [];
+	for (let i = 0; i < len; i++) {
+		thisChar = number.charAt(i);
 
-			// Only calculate each number once
-			if (tempAr.indexOf(thisChar) === -1) {
-			
-				// Check for the amount of occurences
-				for (k = (j + 1); k < len; k++) {
-					if (thisChar === thisNumber.charAt(k)) {
-						array[i].score += 1;
-					}
+		// Only calculate each number once
+		if (tempAr.indexOf(thisChar) === -1) {
+		
+			// Check for the amount of occurences
+			for (let j = (i + 1); j < len; j++) {
+				if (thisChar === number.charAt(j)) {
+					score += 1;
 				}
 			}
-			
-			// Check for adjacency
-			if (thisChar === thisNumber.charAt(j + 1)) {
-				array[i].score += 2;
-			}
-
-			tempAr.push(thisChar);
-			
 		}
+		
+		// Check for adjacency
+		if (thisChar === number.charAt(i + 1)) {
+			score += 2;
+		}
+
+		tempAr.push(thisChar);
 	}
 
-	sortedNumbers = array.sort( compare );
-	return sortedNumbers;
+	return score
 }
 
 function compare(a, b) {
