c1c47f7480224eca9457be0096915d7fa1ff232a
Tuomas Arokanto <tuomas.arokanto@gmail.com>
2021-10-22 12:55:31 +0300

    Add Prettier and allow it to format all documents

--- good-phone-numbers.js
+++ good-phone-numbers.js
@@ -1,57 +1,55 @@
 function generatePhoneNumbers(amount, start, end) {
-	let numbers = [], i;
-	for (i = 0; i < amount; i++) {
-		numbers[i] = String(Math.floor(Math.random() * (end - start)) + start + 1);
-	}
-	return numbers;
+  let numbers = [],
+    i;
+  for (i = 0; i < amount; i++) {
+    numbers[i] = String(Math.floor(Math.random() * (end - start)) + start + 1);
+  }
+  return numbers;
 }
 
 function calculateScores(numbers) {
-	const scored = numbers.map(number => {
-		return {
-			score: calculateScore(number),
-			num: number
-		}
-	})
+  const scored = numbers.map((number) => {
+    return {
+      score: calculateScore(number),
+      num: number,
+    };
+  });
 
-	const sorted = scored.sort(compare)
+  const sorted = scored.sort(compare);
 
-	return sorted
+  return sorted;
 }
 
 function calculateScore(number) {
-	const len = number.length
-	let score = 0
-	tempAr = [];
-	for (let i = 0; i < len; i++) {
-		thisChar = number.charAt(i);
+  const len = number.length;
+  let score = 0;
+  tempAr = [];
+  for (let i = 0; i < len; i++) {
+    thisChar = number.charAt(i);
 
-		// Only calculate each number once
-		if (tempAr.indexOf(thisChar) === -1) {
-		
-			// Check for the amount of occurences
-			for (let j = (i + 1); j < len; j++) {
-				if (thisChar === number.charAt(j)) {
-					score += 1;
-				}
-			}
-		}
-		
-		// Check for adjacency
-		if (thisChar === number.charAt(i + 1)) {
-			score += 2;
-		}
+    // Only calculate each number once
+    if (tempAr.indexOf(thisChar) === -1) {
+      // Check for the amount of occurences
+      for (let j = i + 1; j < len; j++) {
+        if (thisChar === number.charAt(j)) {
+          score += 1;
+        }
+      }
+    }
 
-		tempAr.push(thisChar);
-	}
+    // Check for adjacency
+    if (thisChar === number.charAt(i + 1)) {
+      score += 2;
+    }
 
-	return score
+    tempAr.push(thisChar);
+  }
+
+  return score;
 }
 
 function compare(a, b) {
-	if (a.score > b.score)
-		return -1;
-	if (a.score < b.score)
-		return 1;
-	return 0;
+  if (a.score > b.score) return -1;
+  if (a.score < b.score) return 1;
+  return 0;
 }
